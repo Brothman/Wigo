0c6673e119dc389524c9e604777ead4024ebddc0
Ben Rothman <brothman7000@gmail.com>
2017-06-22 21:04:54 +0200

    Tried firebase database, failed firebase database, made search page link to venue page

--- src/components/RestaurantSearchDatabase/index.js
+++ src/components/RestaurantSearchDatabase/index.js
@@ -12,7 +12,7 @@ import clubs from '../../utils/ClubsDataTemplate.js';
 import events from '../../utils/EventsDataTemplate.js';
 import fitness from '../../utils/Fitness.js';
 
-import {firebaseDb} from '../../utils/firebase';
+
 
 
 class RestaurantDatabase extends React.Component {
@@ -32,7 +32,6 @@ class RestaurantDatabase extends React.Component {
 
 
 
-
   searchAlorithimStateChange = (arrayToLoop, searchTerm) => {
     //Allows us to keep old finds in the resultArray
     var resultArray = this.state.resultArray;
@@ -43,7 +42,6 @@ class RestaurantDatabase extends React.Component {
       if (searchTerm.toUpperCase() == restaurant.name.toUpperCase()) {
         resultArray.push(restaurant);
       }
-
       //Check if the search matches a tag
       restaurant.tags.forEach((tag) => {
         if (searchTerm.toUpperCase() == tag.toUpperCase()) {
@@ -88,7 +86,6 @@ class RestaurantDatabase extends React.Component {
     this.searchAlorithimStateChange(this.state.EventsArray, searchTerm);
     this.searchAlorithimStateChange(this.state.FitnessArray, searchTerm);
     this.shuffleArray();
-  //  console.log(this.state.EventsArray);
   }
 
 /*  addDatabaseInformation = () => {
@@ -99,9 +96,11 @@ class RestaurantDatabase extends React.Component {
     })
   }*/
 
+
   componentWillMount = () => {
-      this.searchAlgorithim(this.props.params.searchQuery);
+    this.searchAlgorithim(this.props.params.searchQuery);
   }
+
 
   componentWillReceiveProps = (nextProps) => {
     //call searchAlgorithim once resultArray has been reset for new search
@@ -109,24 +108,57 @@ class RestaurantDatabase extends React.Component {
       this.searchAlgorithim(nextProps.params.searchQuery)})
   }
 
+/* I tried, I really tried. RIP Firebase Database
 
-    render() {
-    //  var database = firebaseApp.database().ref('Activities');
-    //  console.log('data activites', firebaseDb.ref('Activities'));
-    //  console.log('data', firebaseDb);
-    firebaseDb.ref('Activities').once('value').then((data) => {
+  changeData = () => {
+    var ActivityArray = this.changeDataFunctionality('Activities');
+    var EventArray = this.changeDataFunctionality('Events');
+    var ExploreArray = this.changeDataFunctionality('Explore');
+    var FitnessArray = this.changeDataFunctionality('Fitness');
+    var PlacesArray = this.changeDataFunctionality('Places');
+    var RestaurantArray = this.changeDataFunctionality('Restaurants');
+    this.setState ({
+      ActivityArray: ActivityArray,
+      EventArray: EventArray,
+      ExploreArray: ExploreArray,
+      FitnessArray: FitnessArray,
+      PlacesArray: PlacesArray,
+      RestaurantArray: RestaurantArray
+    }, function () {
+      //console.log("name please", this.state.ActivityArray['0'])
+      //this.searchAlgorithim(this.props.params.searchQuery);
+    //  console.log('state', this.state);
+    }
+      )
+  }
+
+
+   changeDataFunctionality = (tag) => {
+     //array to store all the value
+     var storageArray = [];
+
+    firebaseDb.ref(tag).once('value').then((data) => {
       var dataValue = data.val();
-      console.log('data.val', dataValue);
-      //array to store all the value
-      var storageArray = [];
+      //console.log('data.val', dataValue);
+
       //make an array of all the different places
       for (var key in dataValue) {
         if (dataValue.hasOwnProperty(key)) {
     storageArray.push(dataValue[key]);
   }
-}
+  }
     })
+    console.log('storageArray', storageArray)
+    console.log('storageArray[0]', storageArray[0])
+    return storageArray;
+  }
 
+*/
+
+
+
+
+    render() {
       return (
         <div className="search-restaurant-container">
           {this.state.resultArray.map(function(restaurant, i) {
